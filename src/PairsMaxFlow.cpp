1e3a941dead25abbb2eb8a6aa0605848a7936acc
António Azevedo <antazevedo2003@gmail.com>
2023-04-05 02:19:46 +0100

    Added functionality for maxflow pair menu and function

--- src/PairsMaxFlow.cpp
+++ src/PairsMaxFlow.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "PairsMaxFlow.h"
+#include <iomanip>
 
 PairsMaxFlow::PairsMaxFlow(int &currMenuPage, Graph &gh) : MenuItem(currMenuPage, gh){
     this->graph = gh;
@@ -14,6 +15,7 @@ bool sortResultVector(const pair<pair<Vertex*, Vertex*>, int> &p1, const pair<pa
 
 void PairsMaxFlow::execute() {
     auto extremes = graph.getExtremes();
+    result_vector.clear();
     int flow = 0;
     for (auto v1: extremes){
         for (auto v2: extremes){
@@ -25,6 +27,19 @@ void PairsMaxFlow::execute() {
         }
     }
     std::sort(result_vector.begin(), result_vector.end(), sortResultVector);
-    cout << "stations: " << result_vector[0].first.first->getId() << ", " << result_vector[0].first.second->getId() << endl;
-    cout << "Max Flow: " << result_vector[0].second << endl;
+
+    cout << " _____________________________________________ \n"
+         << "|\033[40m                   Results                   \033[0m|\n"
+         << "|\033[40m_____________________________________________\033[0m|\n";
+    cout << "|\033[100m Max-Flow: " << result_vector[0].second << "                                \033[0m|\n"
+         << "|\033[100m\033[37m---------------------------------------------\033[0m|\n";
+    cout << "|\033[100m Pairs with Max-Flow:                        \033[0m|\n";
+
+    for (auto pa: result_vector) {
+        auto s = pa.first.first->getId() + " <-> " + pa.first.second->getId();
+        cout.clear();
+        if (pa.second == result_vector[6].second) cout << "|\033[100m   -->   " << s << string(36 - s.size(), ' ') << "\033[0m|\n";
+    }
+    string useless;
+    cin >> useless;
 }
